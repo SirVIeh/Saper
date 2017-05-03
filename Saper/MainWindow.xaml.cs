ae49c0abc09d4f602003b322561b94622a62b1e2
Vieh <sirvieh@gmail.com>
2017-05-03 18:41:31 +0200

    Added buttons

--- Saper/MainWindow.xaml.cs
+++ Saper/MainWindow.xaml.cs
@@ -20,9 +20,45 @@ namespace Saper
     /// </summary>
     public partial class MainWindow : Window
     {
+        public enum Level { Easy, Medium, Hard};
+        public static Level Difficulty = Level.Easy;
         public MainWindow()
         {
             InitializeComponent();
+            int columnCount = 0;
+            switch (Difficulty)
+            {
+                case Level.Easy:
+                    columnCount = 10;
+                    break;
+                case Level.Medium:
+                    columnCount = 20;
+                    break;
+                case Level.Hard:
+                    columnCount = 30;
+                    break;
+            }
+
+            for (int i = 0; i < columnCount; i++)
+            {
+                ColumnDefinition column = new ColumnDefinition();
+                column.Width = new GridLength(1, GridUnitType.Star);
+                MainGrid.ColumnDefinitions.Add(column);
+                RowDefinition row = new RowDefinition();
+                row.Height = new GridLength(1, GridUnitType.Star);
+                MainGrid.RowDefinitions.Add(row);
+            }
+            for (int i = 0; i < columnCount; i++)
+            {
+                for (int j = 0; j < columnCount; j++)
+                {
+                    Button button = new Button();
+                    button.Name = string.Format("{0}{1}", i, j);
+                    Grid.SetRow(button, i);
+                    Grid.SetColumn(button, j);
+                    MainGrid.Children.Add(button);
+                }
+            }
         }
     }
 }
